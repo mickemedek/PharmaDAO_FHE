8f34e4d73eb9ded3d794215d908931f35496b31a
mickemedek <mickemedek@users.noreply.github.com>
2025-11-13 11:15:37 +0000

    Add product.rs, bridge-utils-static.go, configuration-global.jsx, message.adoc

---
+++ auth/product.rs
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/pharmadao_fhe/auth/product.rs
+// Generated at: 2025-11-27T12:25:21.788Z
+
+
---
+++ auth/shared-ape-admin/bridge-utils-static.go
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/pharmadao_fhe/auth/shared-ape-admin/bridge-utils-static.go
+// Generated at: 2025-11-27T12:25:21.788Z
+
+
